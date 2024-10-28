cdaebac59645c2c96b736a728654b52c71450b67
Juan Nieves <j.nievesh@uniandes.edu.co>
2024-10-27 20:59:05 -0500

    taller1ts

--- scripts/serie.js
+++ scripts/serie.js
@@ -1,16 +1,13 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Serie = void 0;
-var Serie = /** @class */ (function () {
-    function Serie(id, title, network, seasons, description, url, image) {
+class Serie {
+    constructor(id, name, channel, seasons, description, url, image) {
         this.id = id;
-        this.title = title;
-        this.network = network;
+        this.name = name;
+        this.channel = channel;
         this.seasons = seasons;
         this.description = description;
         this.url = url;
         this.image = image;
     }
-    return Serie;
-}());
-exports.Serie = Serie;
+}
+
+export { Serie };
--- serie.ts
+++ serie.ts
@@ -1,16 +1,16 @@
 export class Serie {
     id: number;
-    title: string;
-    network: string;
+    name: string;
+    channel: string;
     seasons: number;
     description: string;
     url: string;
     image: string;
 
-    constructor(id:number , title: string, network: string, seasons: number, description: string, url: string, image: string) {
+    constructor(id:number , name: string, channel: string, seasons: number, description: string, url: string, image: string) {
         this.id = id;
-        this.title = title;
-        this.network = network;
+        this.name = name;
+        this.channel = channel;
         this.seasons = seasons;
         this.description = description;
         this.url = url;
